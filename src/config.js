9fc8afa5ca8a78ef9fa19d684a6890221c7be14a
Polaris <tunglamnguyen91@gmail.com>
2021-09-14 18:53:06 +0700

    init

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+    "TOKEN_ENDPOINT": "https://wso2-is.hanhchinhcong.net/oauth2/token",
+    "AUTHORIZE_ENDPOINT": "https://wso2-is.hanhchinhcong.net/oauth2/authorize",
+    "RESPONSE_TYPE": "code",
+    "SCOPE": "openid",
+    "REDIRECT_URI": "https://localhost:9000/login",
+    "CLIENT_ID": "rKoDxA18Jqk1kyrSbZCFP9s7W1Ya",
+    "CLIENT_SECRET": "_Ge4RrmksvRDMVc5059o8ej5ZfAa",
+    "GRANT_TYPE": "authorization_code",
+    "CLIENT_URL": "https://localhost:9000",
+    "LOGOUT_URL": "https://wso2-is.hanhchinhcong.net/oidc/logout",
+    "COOKIE_PATH": "/"
+};
