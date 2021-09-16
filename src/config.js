f3988cf643dcab4612d5af2d576a76fffab34bf0
Polaris <tunglamnguyen91@gmail.com>
2021-09-16 16:56:50 +0700

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const CONFIG = {
     "TOKEN_ENDPOINT": "https://wso2-is.hanhchinhcong.net/oauth2/token",
     "AUTHORIZE_ENDPOINT": "https://wso2-is.hanhchinhcong.net/oauth2/authorize",
     "RESPONSE_TYPE": "code",
-    "SCOPE": "openid",
+    "SCOPE": "address phone profile openid email",
     "REDIRECT_URI": "https://localhost:9000/login",
     "CLIENT_ID": "rKoDxA18Jqk1kyrSbZCFP9s7W1Ya",
     "CLIENT_SECRET": "_Ge4RrmksvRDMVc5059o8ej5ZfAa",
